c68f0ca63f35acf94b8f7128beae995b3c7cafde
Herb McNew <github.com@herbmcnew.com>
2022-08-06 00:05:59 -0400

    Update __init__.py
    Change over to a uart component.

--- components/leapmmw/__init__.py
+++ components/leapmmw/__init__.py
@@ -1,14 +1,18 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import uart
 from esphome.const import CONF_ID
 
+DEPENDENCIES = ['uart']
+
 leapmmw_ns = cg.esphome_ns.namespace('leapmmw')
-LEAPMMW = leapmmw_ns.class_('LEAPMMW', cg.Component)
+LEAPMMW = leapmmw_ns.class_('LEAPMMW', cg.Component, uart.UARTDevice)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(LEAPMMW)
-}).extend(cv.COMPONENT_SCHEMA)
+}).extend(cv.COMPONENT_SCHEMA).extend(uart.UART_DEVICE_SCHEMA)
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     yield cg.register_component(var, config)
+    yield uart.register_uart_device(var, config)
