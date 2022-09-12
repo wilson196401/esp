2fe03bf213f6bc8a492a1d2a560967bf7b8645e0
Herb McNew <github.com@herbmcnew.com>
2022-09-12 01:48:18 -0400

    First brute force attempt
    First attempt at massaging the existing code directly into a custom component. Not expected to work at this stage.

--- components/hs2xx3a/hs2xx3a.h
+++ components/hs2xx3a/hs2xx3a.h
@@ -7,12 +7,20 @@ namespace esphome {
 namespace hs2xx3a {
 
 class HS2XX3A : public uart::UARTDevice, public Component {
-  public:
-    void setup() override;
-    void loop() override;
-    void dump_config() override;
-};
+ public:
+  void setup() override;
+  void loop() override;
+  void dump_config() override;
 
+  void publishTarget(std::string idx, float dist, float snr);
+  static void clearTargets();
+  void publishNumber(std::string sensor, float resp);
+  void publishSwitch(std::string sensor, int state);
+  void getmmwConf(std::string mmwparam);
+
+ protected:
+  int readline(int readch, char *buffer, int len);
+};
 
 }  // namespace hs2xx3a
 }  // namespace esphome
